825100d44ec88b4d618f88f8d83cb8235802ea70
suhas023 <notsuhas@gmail.com>
2018-11-09 23:22:47 +0530

    service added

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 import { GlobeService } from '../globe.service';
-import {Filter} from '../filter';
+import {Filter} from '../interfaces/filter.interface';
 
 @Component({
   selector: 'app-countries',
@@ -11,19 +12,23 @@ import {Filter} from '../filter';
   styleUrls: ['./countries.component.css']
 })
 export class ContriesComponent implements OnInit {
-  countries: any[] = [];
-  filter: Filter;
+  countries$: any;
+  filter$: Observable<Filter>;
   constructor(private globeService: GlobeService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.route.queryParamMap
-      .subscribe(params => {
-        this.filter = {
-          category: params.get('category'),
-          value: params.get('value')
-        }
+
+    this.filter$ = this.route.queryParamMap
+      .pipe(map(params => ({
+        category: params.get('category'),
+        value: params.get('value')
+      })));
+    
+    this.filter$
+      .subscribe((filter:Filter) => {
+        this.countries$ = this.globeService.getCountries(filter);
       });
-    console.log(this.filter);
+      
   }
 }
--- src/app/globe.service.ts
+++ src/app/globe.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Filter } from './interfaces/filter.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobeService {
 
-  apiURL: string = 'https://restcountries.eu/rest/v2/';
-  acceptableFilters = ['africa', 'americas', 'asia', 'europe', 'oceania', 'lang', 'currency'];
+  apiURL: string = 'https://restcountries.eu/rest/v2';
+  acceptableFilters = ['region', 'lang', 'currency'];
 
   constructor(private http: HttpClient) {
+  }
+
+  getCountries(filter: Filter):Observable<HttpResponse<any>> {
+    let url = `${this.apiURL}/${filter.category}/${filter.value}`;
+    return this.http.get<any>(url);
   }
 
 
